458862f9caf86818e212a95b0e7a510a2dc7bcc4
Aishwaryalonkar <aishwarya.lonkar@nanobnk.com>
2018-02-06 21:31:15 +0530

    s

--- abc.com/client/src/app/globals/globals.ts
+++ abc.com/client/src/app/globals/globals.ts
@@ -1,12 +1,9 @@
-/**
- * Created by gipai on 10/8/2017.
- */
 import {Injectable} from "@angular/core";
 
 @Injectable()
 export class Globals{
 
-  private backEndUrl = 'http://localhost:8080/api/';
+  private backEndUrl = 'http://97.74.237.176:8080/api/';
 
   public getBackendUrl() : string {
     return this.backEndUrl;
